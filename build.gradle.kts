23fb4fe2b737bc5ee714200c1c6385d314ad773d
masteryyh <yyh991013@hotmail.com>
2022-03-26 17:32:07 +0800

    feature: added some dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,11 @@ dependencies {
     implementation("org.xerial:sqlite-jdbc:3.36.0.3")
     implementation("org.postgresql:postgresql:42.3.3")
     implementation("mysql:mysql-connector-java:8.0.28")
+
+    // Logging
+    implementation("org.apache.logging.log4j:log4j-core:2.17.2")
+    implementation("org.apache.logging.log4j:log4j-api:2.17.2")
+    implementation("org.slf4j:slf4j-api:1.7.36")
 }
 
 tasks.getByName<Test>("test") {
