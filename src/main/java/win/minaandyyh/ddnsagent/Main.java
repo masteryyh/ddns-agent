2f2717143987c38f2b984a1a4a4cc3b198345865
masteryyh <9990804+masteryyh@users.noreply.github.com>
2022-04-06 12:54:36 +0800

    feature: main class placeholder

---
+++ src/main/java/win/minaandyyh/ddnsagent/Main.java
@@ -0,0 +1,20 @@
+package win.minaandyyh.ddnsagent;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Service;
+
+/**
+ * Main class
+ *
+ * @author masteryyh
+ */
+@Service
+@ComponentScan(basePackages = "win.minaandyyh.ddnsagent")
+public class Main {
+    public static void main(String[] args) {
+        try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Main.class)) {
+            System.out.println(context.getApplicationName());
+        }
+    }
+}
