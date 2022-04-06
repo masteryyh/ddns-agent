5d1d17913e8b4184ca02ecaea46ea8e8dcca64b5
masteryyh <9990804+masteryyh@users.noreply.github.com>
2022-04-06 18:17:52 +0800

    feature: basic unit tests (not done yet)

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,6 +47,10 @@ dependencies {
     implementation("org.apache.logging.log4j:log4j-core:2.17.2")
     implementation("org.apache.logging.log4j:log4j-api:2.17.2")
     implementation("org.slf4j:slf4j-api:1.7.36")
+
+    // Test framework
+    testImplementation("org.testng:testng:7.5")
+    testImplementation("org.springframework:spring-test:5.3.18")
 }
 
 tasks.getByName<Test>("test") {
